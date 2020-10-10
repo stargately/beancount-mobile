eb343a89c9da438ee6b55be26f5544f3a16f82ac
BigBug <yuyongmao@126.com>
2020-10-10 15:18:14 +0800

    feat: add register push token

---
+++ src/common/push-token/use-push-token-to-remote.ts
@@ -0,0 +1,15 @@
+import { useMutation } from "react-apollo";
+import {
+  addPushToken,
+  addPushTokenVariables,
+} from "@/common/push-token/data/__generated__/addPushToken";
+import { addPushTokenToRemote } from "@/common/push-token/data/mutations";
+
+export const useAddPushTokenToRemote = () => {
+  const [mutate, { error, data }] = useMutation<
+    addPushToken,
+    addPushTokenVariables
+  >(addPushTokenToRemote);
+
+  return { error, mutate, data };
+};
--- src/common/register-push-token.tsx
+++ src/common/register-push-token.tsx
@@ -1,13 +1,6 @@
-// import { Notifications } from "expo";
+import { Notifications } from "expo";
 import * as Permissions from "expo-permissions";
-// import gql from "graphql-tag";
-// import { apolloClient } from "@/common/apollo-client";
-
-// export const ADD_PUSH_TOKEN = gql`
-//   mutation addPushToken($pushToken: String) {
-//     addPushToken(token: $pushToken)
-//   }
-// `;
+import { useAddPushTokenToRemote } from "@/common/push-token/use-push-token-to-remote";
 
 export const registerForPushNotificationAsync = async (): Promise<string> => {
   const { status: existingStatus } = await Permissions.getAsync(
@@ -21,19 +14,20 @@ export const registerForPushNotificationAsync = async (): Promise<string> => {
   if (finalStatus !== "granted") {
     return finalStatus;
   }
-  // const pushToken = await Notifications.getExpoPushTokenAsync();
-  // if (pushToken) {
-  //   try {
-  //     await apolloClient.mutate({
-  //       mutation: ADD_PUSH_TOKEN,
-  //       variables: {
-  //         pushToken
-  //       }
-  //     });
-  //   } catch (error) {
-  //     // tslint:disable-next-line
-  //     console.log(`add push token fail ${error}`);
-  //   }
-  // }
+  const pushToken = await Notifications.getExpoPushTokenAsync();
+
+  if (pushToken) {
+    try {
+      const { mutate, error } = useAddPushTokenToRemote();
+      await mutate({ variables: { pushToken } });
+      if (error) {
+        // tslint:disable-next-line
+        console.log(`add push token fail ${error}`);
+      }
+    } catch (e) {
+      // tslint:disable-next-line
+      console.log(`add push token fail ${e}`);
+    }
+  }
   return finalStatus;
 };
