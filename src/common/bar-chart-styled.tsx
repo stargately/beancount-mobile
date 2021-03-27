2032909ac638a247f6b8077d88a383f64872e136
Tian Pan <puncsky@gmail.com>
2021-03-27 14:23:06 -0700

    Update bar-chart-styled.tsx

--- src/common/bar-chart-styled.tsx
+++ src/common/bar-chart-styled.tsx
@@ -3,9 +3,8 @@ import { BarChart } from "@/common/bar-chart";
 import { contentPadding, ScreenWidth } from "@/common/screen-util";
 import { useTheme } from "@/common/theme";
 import { i18n } from "@/translations";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import shortNumber from "short-number";
+
+const shortNumber = require("short-number");
 
 export function BarChartStyled({
   labels,
