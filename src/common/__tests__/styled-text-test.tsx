05dda8c178657b00e2bd37967cdefde245e83ebd
kwoktung <onlyif2009@gmail.com>
2025-06-17 12:36:20 +0800

    delete useless code (#61)

--- src/common/__tests__/styled-text-test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as renderer from "react-test-renderer";
-import { MonoText } from "@/common/styled-text";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
