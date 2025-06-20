be9089950f2dd596c3fa61d82cdd6fa5a9043b8f
kwoktung <onlyif2009@gmail.com>
2025-06-20 09:26:03 +0800

    Add progress component (#75)

---
+++ src/common/hooks/use-debounce.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+const useDebounce = <T>(value: T, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
