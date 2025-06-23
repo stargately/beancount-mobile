1ae8fcc7015560d22062a3791773a8244ada31a8
Tian Pan <puncsky@gmail.com>
2025-06-23 02:18:22 -0700

    Remove unused hooks (#82)

--- src/common/hooks/use-push-token-to-remote.ts
+++
@@ -1,7 +0,0 @@
-import { useAddPushTokenMutation } from "@/generated-graphql/graphql";
-
-export const useAddPushTokenToRemote = () => {
-  const [mutate, { error, data }] = useAddPushTokenMutation();
-
-  return { error, mutate, data };
-};
