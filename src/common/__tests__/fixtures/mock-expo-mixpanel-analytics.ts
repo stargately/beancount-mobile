0dbe3e41c15ba022f28d2bc6fc05cbfe94e02f57
Tian Pan <puncsky@gmail.com>
2025-10-01 14:49:25 -0700

    Add analytics module tests

---
+++ src/common/__tests__/fixtures/mock-expo-mixpanel-analytics.ts
@@ -0,0 +1,28 @@
+export class MockExpoMixpanelAnalytics {
+  static instances: MockExpoMixpanelAnalytics[] = [];
+
+  static reset(): void {
+    MockExpoMixpanelAnalytics.instances = [];
+  }
+
+  token: string;
+
+  identifyCalls: Array<string | undefined> = [];
+
+  trackCalls: Array<{ name: string; props: Record<string, any> }> = [];
+
+  constructor(token: string) {
+    this.token = token;
+    MockExpoMixpanelAnalytics.instances.push(this);
+  }
+
+  identify(id?: string): void {
+    this.identifyCalls.push(id);
+  }
+
+  track(name: string, props: Record<string, any>): void {
+    this.trackCalls.push({ name, props });
+  }
+}
+
+export { MockExpoMixpanelAnalytics as ExpoMixpanelAnalytics };
