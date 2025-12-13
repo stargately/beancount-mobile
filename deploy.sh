ea59e9cb601dcaa722d3dd678c21677ed8c092d0
Dora Noda <38552114+doranoda@users.noreply.github.com>
2025-12-12 20:44:53 -0800

    update

--- deploy.sh
+++ deploy.sh
@@ -7,29 +7,37 @@ set -e # exit on error
 # Install dependencies
 yarn
 
-# Install EAS CLI if not already installed
+# 1. Install npx eas-cli@latest CLI if not already installed
 yarn global add eas-cli@latest
 
 # No need for login; EAS_TOKEN will be used automatically by the CLI if set
 # export EAS_TOKEN=your-token # <-- make sure this is set in your CI or shell environment
 
-#### 2. Send Over-the-Air Updates ####
-eas update --channel production --message "Production update $(date +'%Y-%m-%d %H:%M:%S')"
+# 2. Send Over-the-Air Updates ####
+npx eas-cli@latest update --channel production --message "Production update $(date +'%Y-%m-%d %H:%M:%S')"
+
+# --------------------------------------------------
+# iOS
+# --------------------------------------------------
 
 
-#### 5. Build iOS App ####
+# 3. Build iOS App ####
 echo "Building iOS app..."
-eas build --platform ios --profile production --non-interactive --no-wait
+npx eas-cli@latest build --platform ios --profile production --non-interactive --no-wait
 
-#### 6. Submit iOS App ####
+# 4. Submit iOS App ####
 echo "Submitting iOS app to App Store..."
-eas submit --platform ios --latest --non-interactive
+npx eas-cli@latest submit --platform ios --latest --non-interactive
+
+# --------------------------------------------------
+# android
+# --------------------------------------------------
 
 
-### 3. Build Android App ####
+# 5. Build Android App ####
 echo "Building Android app..."
-eas build --platform android --profile production --non-interactive --no-wait
+npx eas-cli@latest build --platform android --profile production --non-interactive --no-wait
 
-#### 4. Submit Android App ####
+# 6. Submit Android App ####
 echo "Submitting Android app to Play Store..."
-eas submit --platform android --latest --non-interactive
+npx eas-cli@latest submit --platform android --latest --non-interactive
